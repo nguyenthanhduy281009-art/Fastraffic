3eccfb7d6623b5cf583a32f326d3e1f2f558e982
nguyenthanhduy281009-art <nguyenthanhduy281009@gmail.com>
2026-01-03 21:34:31 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,1 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBka80W-Aa9m4NXQ7PD4kmx1xew-lz67l0",
-  authDomain: "fastraffic-8b585.firebaseapp.com",
-  databaseURL: "https://fastraffic-8b585-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fastraffic-8b585",
-  storageBucket: "fastraffic-8b585.appspot.com",
-  messagingSenderId: "124682318080",
-  appId: "1:124682318080:web:f536eacba0881a078683e5",
-  measurementId: "G-5FBQ8W5Z7G"
-};
 
-// BẮT BUỘC PHẢI CÓ
-firebase.initializeApp(firebaseConfig);
